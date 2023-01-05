01a493d5d17db327dacc77177421eafe7a504f9c
chengyu <17891609981@163.com>
2023-01-05 14:52:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Life msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script setup lang="ts">
+import Life from './components/Life.vue'
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
+
---
+++ src/components/Life.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="hello">
+    <div>
+      <button @click="running?()=>{}:start()">start</button>
+      <button @click="stop">stop</button>
+      <div>total:{{ selectList.length }}</div>
+    </div>
+    <div class="main_box" @click="changeSelect" ref="mainBox">
+      <div 
+        v-for="(box,index) in selectList" 
+        :key="box!.x+'-'+box!.y" 
+        v-memo="[box]" 
+        :style="{
+          left: box!.x / maxX * 100 + '%',
+          top: Number(box!.y) / maxY * 100 + '%',
+          width: 1 / maxX * 100 + '%',
+          height: 1 / maxY * 100 + '%'
+        }" 
+        class="items">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect, watch, onBeforeUnmount } from 'vue'
+const mainBox = ref<HTMLInputElement | null>(null)
+let maxX = 200
+let maxY = 200
+let running = ref(false)
+
+//选择集合key为y，Set值为x
+type setList = { [index: string]: Set<number> }
+const selectBox = ref<setList>({
+  '0':new Set([0]),
+  '10': new Set([1, 2,7]),
+  '11':new Set([2,3,6]),
+  '12': new Set([1, 3,  5, 6]),
+  '20': new Set([1, 2, 4,7]),
+  '21':new Set([2,3,5,6]),
+  '22': new Set([1,3,4,5, 6]),
+})
+
+type Select = { x: number, y: number }
+type SelectList = Array<Select>
+//修改单个状态
+function changeOne(select:Select, to:boolean) {
+  if (to) {
+    if (!selectBox.value[select.y]) {
+      selectBox.value[select.y] = new Set([])
+    }
+    selectBox.value[select.y].add(select.x)
+  } else {
+    if (selectBox.value[select.y]) {
+      selectBox.value[select.y].delete(select.x)
+      if (selectBox.value[select.y].size == 0) {
+        delete selectBox.value[select.y]
+      }
+    }
+  }
+}
+function changeSelect(e:MouseEvent) {
+  let x = e.x - mainBox.value!.getBoundingClientRect().x
+  let y = e.y - mainBox.value!.getBoundingClientRect().y
+  let xNum = Math.floor(x / mainBox.value!.offsetWidth * maxX)
+  let yNum = Math.floor(y / mainBox.value!.offsetHeight * maxY)
+  changeOne({x:xNum,y:yNum},!(selectBox.value[yNum]&&selectBox.value[yNum].has(xNum)))
+}
+//根据选择集合修改dataList
+const selectList = ref<SelectList>([])
+watchEffect(() => {
+  const list:SelectList = []
+  Object.keys(selectBox.value).forEach(y => {
+    selectBox.value[y].forEach(x => {
+      list.push({x,y:Number(y)})
+    })
+  })
+  selectList.value = [...list]
+})
+//计时器两种方案不知道有没有区别
+// async function delayFn(delay:number):Promise<any> {
+//   await new Promise(function (res:Function, reject) {
+//     setTimeout(() => {
+//       let allBox:setList = {}
+//       Object.keys(selectBox.value).forEach(y => {
+//         selectBox.value[y].forEach(x => {
+//           for (let j = x - 1; j <= x + 1; j++){
+//             for (let k = Number(y) - 1; k <= Number(y) + 1; k++){
+//               if ((j >= 0) && (k >= 0) && (j<=maxX) && (k<=maxY)) {
+//                 if (!allBox[k]) {
+//                   allBox[k] = new Set([])
+//                 }
+//                 allBox[k].add(j)
+//               }
+//             }
+//           }
+//         })
+//       })
+//       // console.log("ALL", allBox)
+//       Object.keys(allBox).forEach(y => {
+//         allBox[y].forEach(x => {
+//           let count = 0
+//           for (let j = x - 1; j <= x+1; j++){
+//             for (let k = Number(y) - 1; k <= Number(y) + 1; k++){
+//               if ((j >= 0) && (k >= 0) && (j <= maxX) && (k <= maxY) && ((j != x) || (k != Number(y)))) {
+//                 if (selectBox.value[k]&&selectBox.value[k].has(j)) {
+//                   count++
+//                 }
+//                 // if (dataList.value[k][j].selected) {
+//                 //   count++
+//                 // }
+//               }
+//             }
+//           }
+//           if (count == 3) {
+//             changeOne({ x,y:Number(y) },true)
+//           } else if((count >= 4)||(count <= 1)) {
+//             changeOne({ x,y:Number(y) },false)
+//           }
+//         })
+//       })
+//       res()
+//     },delay)
+//   })
+// }
+// async function start(res?:Promise<any>) {
+//   if (res) {
+//     res.then(async () => {
+//       res = await delayFn(10)
+//     })
+//   } else {
+//     res = await delayFn(10)
+//   }
+//   start()
+//   return res
+// }
+const t = ref<number>(0)
+function start() {
+  t.value = setInterval(() => {
+    running.value = true
+    let allBox:setList = {}
+    Object.keys(selectBox.value).forEach(y => {
+      selectBox.value[y].forEach(x => {
+        for (let j = x - 1; j <= x + 1; j++){
+          for (let k = Number(y) - 1; k <= Number(y) + 1; k++){
+            if ((j >= 0) && (k >= 0) && (j<=maxX) && (k<=maxY)) {
+              if (!allBox[k]) {
+                allBox[k] = new Set([])
+              }
+              allBox[k].add(j)
+            }
+          }
+        }
+      })
+    })
+    Object.keys(allBox).forEach(y => {
+      allBox[y].forEach(x => {
+        let count = 0
+        for (let j = x - 1; j <= x+1; j++){
+          for (let k = Number(y) - 1; k <= Number(y) + 1; k++){
+            if ((j >= 0) && (k >= 0) && (j <= maxX) && (k <= maxY) && ((j != x) || (k != Number(y)))) {
+              if (selectBox.value[k]&&selectBox.value[k].has(j)) {
+                count++
+              }
+            }
+          }
+        }
+        if (count == 3) {
+          changeOne({ x,y:Number(y) },true)
+        } else if((count >= 4)||(count <= 1)) {
+          changeOne({ x,y:Number(y) },false)
+        }
+      })
+    })
+  },50)
+}
+function stop() {
+  running.value = false
+  clearInterval(t.value)
+}
+onBeforeUnmount(() => {
+  clearInterval(t.value)
+})
+</script>
+
+<style scoped>
+.main_box {
+  width: 100%;
+  padding-bottom: 100%;
+  position: relative;
+}
+.items{
+  background-color: aqua;
+  position: absolute;
+}
+
+.flex {
+  display: flex;
+}
+.item {
+  flex: 1;
+  /* padding-bottom: 10px; */
+  display: flex;
+  cursor: pointer;
+}
+.item::after{
+  content: '';
+  padding-top: 100%;
+}
+.active {
+  background-color: aqua;
+}
+</style>
+
